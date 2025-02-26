c3bacf938d82fc37fb5d10846648896e584553ce
idityaGE <am44910606@gmail.com>
2025-02-27 01:48:33 +0530

    Update dependencies and modify hashString logging

--- 00-Orientation/hashsing/main.ts
+++ 00-Orientation/hashsing/main.ts
@@ -1,7 +1,9 @@
 import CryptoJS from "npm:crypto-js@4.2.0";
+import * as console from "node:console";
+
 
 export function hashString(input: string): string {
   return CryptoJS.SHA256(input).toString();
 }
 
-console.log(hashString('hello world'));
+console.log(hashString('Aditya'));

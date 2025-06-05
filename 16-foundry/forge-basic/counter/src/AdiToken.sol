231a9fdfb18b3c8b8afee5f638b8157d32163b1d
idityaGE <am44910606@gmail.com>
2025-06-05 23:12:03 +0530

    refactor: streamline constructor and enhance AdiToken tests with event emissions and additional functionality

--- 16-foundry/forge-basic/counter/src/AdiToken.sol
+++ 16-foundry/forge-basic/counter/src/AdiToken.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.13;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+// import {console} from "forge-std/console.sol";
 
 contract AdiToken is ERC20, Ownable {
-    constructor(
-        uint256 _initSupply
-    ) ERC20("Adi Token", "ADI") Ownable(msg.sender) {
+    constructor(uint256 _initSupply) ERC20("Adi Token", "ADI") Ownable(msg.sender) {
+        // console.log("Adi Token contract initialized"); // use `-vv` two verbosity
         _mint(msg.sender, _initSupply);
     }
 
@@ -17,5 +17,11 @@ contract AdiToken is ERC20, Ownable {
 
     function burn(address from, uint256 amount) external onlyOwner {
         _burn(from, amount);
+    }
+
+    function sendEthToContract() public payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
--- 16-foundry/forge-basic/counter/test/AdiToken.t.sol
+++ 16-foundry/forge-basic/counter/test/AdiToken.t.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {AdiToken} from "../src/AdiToken.sol";
 
+// try `forge test -vvvv`
+// no. of v means level of verbosity
+
 contract AdiTokenTest is Test {
     AdiToken private c;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     function setUp() public {
         c = new AdiToken(1000); // Initialize with 1000 tokens
@@ -28,11 +33,15 @@ contract AdiTokenTest is Test {
         c.mint(owner, 500);
 
         uint256 balanceAfter = c.balanceOf(owner);
-        assertEq(
-            balanceAfter,
-            balanceBefore + 500,
-            "Owner's balance should increase by 500"
-        );
+        assertEq(balanceAfter, balanceBefore + 500, "Owner's balance should increase by 500");
+
+        vm.prank(address(1137)); // next call will be called by address(1137)
+        vm.expectRevert();
+        c.mint(address(1198), 5000); // this will be called by address(1137)
+
+        // now this will be called by address(this), which owner
+        c.mint(owner, 200);
+        assertEq(c.balanceOf(owner), 1700);
     }
 
     function testBurning() public {
@@ -42,11 +51,7 @@ contract AdiTokenTest is Test {
         c.burn(owner, 200);
 
         uint256 balanceAfter = c.balanceOf(owner);
-        assertEq(
-            balanceAfter,
-            balanceBefore - 200,
-            "Owner's balance should decrease by 200"
-        );
+        assertEq(balanceAfter, balanceBefore - 200, "Owner's balance should decrease by 200");
     }
 
     function testRenounceOwnership() public {
@@ -56,9 +61,73 @@ contract AdiTokenTest is Test {
 
     function testTransfer() public {
         uint256 balanceBefore = c.balanceOf(address(this));
+
+        // 1. Setup the expectation
+        vm.expectEmit(true, true, true, true);
+        // 2. Emit the expected event manually
+        emit Transfer(address(this), address(1137), 50);
+        // 3. Call the function that should emit the event
         c.transfer(address(1137), 50);
+
         uint256 balanceAfter = c.balanceOf(address(this));
-        console.log(balanceAfter);
+        console.logUint(balanceAfter);
         assertEq(balanceAfter, balanceBefore - 50);
+        assertEq(c.balanceOf(address(1137)), 50);
+
+        vm.prank(address(1137));
+        c.transfer(address(this), 40);
+
+        assertEq(c.balanceOf(address(1137)), 10);
     }
+
+    function testAllownace() public {
+        address spender = address(1137);
+        address payer = address(this);
+        c.approve(spender, 200);
+        uint256 allowance = c.allowance(payer, spender);
+        assertEq(allowance, 200);
+
+        vm.prank(spender);
+        c.transferFrom(payer, address(1192), 150);
+
+        assertEq(c.balanceOf(payer), 1000 - 150);
+        assertEq(c.balanceOf(address(1192)), 150);
+    }
+
+    function testDeal() public {
+        vm.deal(address(this), 10 ether);
+        assertEq(address(this).balance, 10 ether);
+    }
+
+    function testHoax() public {
+        hoax(address(9990), 10 ether);
+        c.sendEthToContract{value: 5 ether}(); // sending ether to a payable function
+        assertEq(c.getBalance(), 5 ether);
+    }
+
+    /**
+     * #=== Prank ===#
+     * vm.prank(msgSender); only for one next transtion
+     * // transaction 1
+     *
+     * vm.startPrank(msgSender);
+     * // transaction 1
+     * // transaction 2
+     * vm.stopPrank();
+     *
+     *
+     * #=== Deal ===#
+     * vm.deal(account, newBalance);
+     * // set the balance of this `account` to `newBalance`; eg. ->
+     * vm.deal(address(this), 10 ether);
+     *
+     * #=== Hoax ===#
+     * // combinaion of deal and prank
+     * hoax(address msgSender, uint256 give);
+     * // internally
+     * function hoax(address msgSender, uint256 give) internal virtual {
+     *     vm.deal(msgSender, give);
+     *     vm.prank(msgSender);
+     * }
+     */
 }

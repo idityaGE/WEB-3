4e0d49991a48bdbc4735a8eb5681286802cca134
idityaGE <am44910606@gmail.com>
2025-06-05 21:06:08 +0530

    feat: update AdiToken contract with minting and burning functions; enhance tests for ownership, minting, burning, and transfer

--- 16-foundry/forge-basic/counter/src/AdiToken.sol
+++ 16-foundry/forge-basic/counter/src/AdiToken.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract AdiToken is ERC20, Ownable {
     constructor(
         uint256 _initSupply
     ) ERC20("Adi Token", "ADI") Ownable(msg.sender) {
         _mint(msg.sender, _initSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
     }
 }
--- 16-foundry/forge-basic/counter/test/AdiToken.t.sol
+++ 16-foundry/forge-basic/counter/test/AdiToken.t.sol
@@ -10,11 +10,55 @@ contract AdiTokenTest is Test {
     function setUp() public {
         c = new AdiToken(1000); // Initialize with 1000 tokens
     }
-    
+
+    function testOwner() public view {
+        // address(this) => is the owner because, it deploys the contract
+        assertEq(address(this), c.owner());
+    }
+
     function testInitialSupply() public view {
         uint256 initialSupply = c.totalSupply();
         assertEq(initialSupply, 1000, "Initial supply should be 1000");
     }
 
-    
+    function testMinting() public {
+        address owner = c.owner();
+        uint256 balanceBefore = c.balanceOf(owner);
+
+        c.mint(owner, 500);
+
+        uint256 balanceAfter = c.balanceOf(owner);
+        assertEq(
+            balanceAfter,
+            balanceBefore + 500,
+            "Owner's balance should increase by 500"
+        );
+    }
+
+    function testBurning() public {
+        address owner = c.owner();
+        uint256 balanceBefore = c.balanceOf(owner);
+
+        c.burn(owner, 200);
+
+        uint256 balanceAfter = c.balanceOf(owner);
+        assertEq(
+            balanceAfter,
+            balanceBefore - 200,
+            "Owner's balance should decrease by 200"
+        );
+    }
+
+    function testRenounceOwnership() public {
+        c.renounceOwnership();
+        assertEq(address(0), c.owner());
+    }
+
+    function testTransfer() public {
+        uint256 balanceBefore = c.balanceOf(address(this));
+        c.transfer(address(1137), 50);
+        uint256 balanceAfter = c.balanceOf(address(this));
+        console.log(balanceAfter);
+        assertEq(balanceAfter, balanceBefore - 50);
+    }
 }

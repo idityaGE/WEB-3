f0f3118ae160a13c99fecf060ff6de83da5e8f69
idityaGE <am44910606@gmail.com>
2025-06-07 23:35:09 +0530

    refactor: standardize function names and improve event emissions in EthBridge contract

--- 18-bridge/src/eth-bridge.sol
+++ 18-bridge/src/eth-bridge.sol
@@ -6,20 +6,40 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EthBridge is Ownable {
+    struct BridgeTransaction {
+        uint256 amount;
+        // uint256 timestamp;
+        // bool processed;
+    }
+
+    mapping(address => BridgeTransaction) public bridgeTransaction;
+    uint256 public totalLocked;
+    uint256 public totalMinted;
     IERC20 public token;
 
-    event LockedADI(address indexed user, uint256 value);
+    event Minted(address indexed user, uint256 value);
+    event Locked(address indexed user, uint256 value);
 
     constructor(address _tokenAddress) Ownable(msg.sender) {
         token = IERC20(_tokenAddress);
     }
 
-    function lockADI(uint256 _amount) external {
-        require(token.transferFrom(msg.sender, address(this), _amount), "Allowance Failed");
-        emit LockedADI(msg.sender, _amount);
+    function lock(uint256 _amount) external {
+        // first user will sign approve function on token contract
+        require(
+            token.transferFrom(msg.sender, address(this), _amount),
+            "Allowance Failed"
+        );
+
+        bridgeTransaction[msg.sender].amount += _amount;
+        totalLocked += _amount;
+
+        emit Locked(msg.sender, _amount);
     }
 
-    function mintADI(address _to, uint256 _amount) external onlyOwner() {
+    function mint(address _to, uint256 _amount) external onlyOwner {
         require(token.transfer(_to, _amount), "Tansfer failed");
+
+        emit Minted(_to, _amount);
     }
 }

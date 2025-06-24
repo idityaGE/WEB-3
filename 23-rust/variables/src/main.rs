0e2f7aedccd91f4725192c49f6c432e2cfff51cb
idityaGE <am44910606@gmail.com>
2025-06-24 13:30:10 +0530

    implement find_first_a function and update main to display index of first 'a'; add sample text file

--- 23-rust/variables/src/main.rs
+++ 23-rust/variables/src/main.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::PI;
+use std::{f32::consts::PI, fs};
 
 const _THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
@@ -35,7 +35,31 @@ impl Shape {
     }
 }
 
+fn find_first_a(s: &String) -> Option<u32> {
+    let mut idx: u32 = 0;
+    for c in s.chars() {
+        idx = idx + 1;
+        if c == 'a' || c == 'A' {
+            return Some(idx);
+        }
+    }
+    None
+}
+
 fn main() {
+    let s = String::from("boom bam");
+    let ans = find_first_a(&s);
+    match ans {
+        Some(ans) => println!("index of first a is : {}", ans),
+        None => println!("no is a is present")
+    }
+
+    let content = fs::read_to_string("a.txt");
+    match content {
+        Ok(content) => print!("{content}"),
+        Err(e) => println!("{}", e),
+    }
+
     let sq = Shape::Square(20.0);
     let cir = Shape::Circle(3.4);
     let rec = Shape::Rectangle(20.0, 40.0);

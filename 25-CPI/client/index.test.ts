1e9b6bd685e79d925a32200740fba27cc1ae1990
idityaGE <am44910606@gmail.com>
2025-07-14 18:29:29 +0530

    boom

---
+++ 25-CPI/client/index.test.ts
@@ -0,0 +1,64 @@
+import { test, expect } from "bun:test";
+import { LiteSVM } from "litesvm";
+import {
+  PublicKey,
+  Transaction,
+  SystemProgram,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import path from "path";
+
+const programId = new PublicKey("DGGsE2UUwJuPVTQsrDoA1Swbp2Gej65cE22kk1zYACnn");
+const dataAccount = new Keypair();
+const payer = new Keypair();
+
+test("one transfer", () => {
+  const svm = new LiteSVM();
+  svm.addProgramFromFile(programId, path.join(__dirname, 'double.so'));
+  svm.airdrop(payer.publicKey, BigInt(LAMPORTS_PER_SOL));
+
+  const space = 4;
+  const lamports = svm.minimumBalanceForRentExemption(BigInt(space));
+
+  const ixs = [
+    SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: dataAccount.publicKey,
+      lamports: Number(lamports),
+      space,
+      programId
+    })
+  ];
+
+  const tx = new Transaction();
+  let blockhash = svm.latestBlockhash();
+  tx.recentBlockhash = blockhash;
+  tx.add(...ixs);
+  tx.sign(payer, dataAccount);
+  svm.sendTransaction(tx);
+
+  const balanceAfter = svm.getBalance(dataAccount.publicKey);
+  expect(balanceAfter).toBe(svm.minimumBalanceForRentExemption(BigInt(4)));
+
+
+  const data = Buffer.from([0, 0, 0, 0])
+  const txn = new TransactionInstruction({
+    keys: [
+      { pubkey: dataAccount.publicKey, isSigner: true, isWritable: true }
+    ],
+    programId,
+    data
+  })
+
+  const tx2 = new Transaction();
+  blockhash = svm.latestBlockhash();
+  tx2.recentBlockhash = blockhash;
+  tx2.add(txn);
+  tx2.sign(payer, dataAccount);
+  svm.sendTransaction(tx2);
+
+  const dataAccountInfo = svm.getAccount(dataAccount.publicKey);
+  expect(dataAccountInfo?.data).toEqual(new Uint8Array([1, 0, 0, 0]));
+});
--- 25-CPI/double-prog/src/lib.rs
+++ 25-CPI/double-prog/src/lib.rs
@@ -6,6 +6,7 @@ use solana_program::{
     entrypoint,
     entrypoint::ProgramResult,
     msg,
+    program_error::ProgramError,
     pubkey::Pubkey,
 };
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -21,6 +22,11 @@ pub fn process_instruction(
 ) -> ProgramResult {
     let mut acc_iter = accounts.iter();
     let data_account = next_account_info(&mut acc_iter)?;
+
+    if data_account.is_signer != true {
+        return Err(ProgramError::InvalidAccountData);
+    }
+
     let mut counter = OnChainData::try_from_slice(&data_account.data.borrow_mut())?;
 
     if counter.count == 0 {
